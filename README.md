139b5b09d5bb75f1220e6ef1eb49a42f60905400
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-03-22 01:09:31 +0000

    feat: add basic arithmetic helper

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # FakeJuliaReleasePkg
 
 Test Julia package for validating release-please Julia support.
+
+## API
+
+- `greet()`
+- `add(a, b)`
+- `description()`
--- src/FakeJuliaReleasePkg.jl
+++ src/FakeJuliaReleasePkg.jl
@@ -1,5 +1,8 @@
 module FakeJuliaReleasePkg
 
 greet() = "hello"
+add(a, b) = a + b
+
+description() = "a fake package used to validate Julia release automation"
 
 end
